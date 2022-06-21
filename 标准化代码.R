cce8dfc5f483eea6a26a5789ac8d0bdb0534c8e8
Wangmulberry <107164052+Wangmulberry@users.noreply.github.com>
2022-06-21 20:20:24 +0800

    Add files via upload

---
+++ 标准化代码.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)data <- read.csv("C:\\Users\\Lenovo\\Desktop\\cell.csv")  #原始数据
+colnames(d)=data[1,]
+da <- t(da)
+pbmc <- CreateSeuratObject(counts = data, project = "pbmc3k")
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+pbmc <- NormalizeData(pbmc)
+head(pbmc)
+d <- pbmc[["RNA"]]@data
+write.csv(d,"C:\\Users\\Lenovo\\Desktop\\bz.csv")
